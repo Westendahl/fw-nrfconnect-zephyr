892b3f3d72ac217ad10b93bb63fee06ed0bb58b5
Alex Tsamakos <alex@actinius.com>
2019-08-09 21:22:03 +0200

    Remove nrf52840_reset.c and related Kconfig and add Kconfig for ext SIM

--- boards/arm/actinius_icarus/board.c
+++ boards/arm/actinius_icarus/board.c
@@ -9,25 +9,29 @@
 
 #define SIM_SELECT_PIN 8
 
-static void select_internal_sim(void)
+static void select_sim(void)
 {
-	struct device* port = device_get_binding(DT_GPIO_P0_DEV_NAME);
-	if (!port) {
-		return;
-	}
+    struct device *port = device_get_binding(DT_GPIO_P0_DEV_NAME);
+    if (!port) {
+        return;
+    }
 
     gpio_pin_configure(port, SIM_SELECT_PIN, GPIO_DIR_OUT);
-    gpio_pin_write(port, SIM_SELECT_PIN, 1);
+    #ifdef CONFIG_BOARD_SELECT_SIM_EXTERNAL
+        gpio_pin_write(port, SIM_SELECT_PIN, 0);
+    #else
+        gpio_pin_write(port, SIM_SELECT_PIN, 1);
+    #endif
 }
 
 static int board_actinius_icarus_init(struct device *dev)
-{	
-	ARG_UNUSED(dev);
+{
+    ARG_UNUSED(dev);
 
-	select_internal_sim();
+    select_sim();
 
-	return 0;
+    return 0;
 }
 
 SYS_INIT(board_actinius_icarus_init, POST_KERNEL,
-	 CONFIG_KERNEL_INIT_PRIORITY_DEFAULT);
+     CONFIG_KERNEL_INIT_PRIORITY_DEFAULT);
