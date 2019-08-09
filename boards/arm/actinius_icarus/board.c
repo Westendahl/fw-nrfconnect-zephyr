bbdf5417fc5b62c626eadb8ffd1f9b0ce2457b53
Alex Tsamakos <alex@actinius.com>
2019-08-09 02:59:56 +0200

    Add support for setting the SIM Select pin on sys init

---
+++ boards/arm/actinius_icarus/board.c
@@ -0,0 +1,33 @@
+/*
+ * Copyright (c) 2019 Actinius
+ *
+ * SPDX-License-Identifier: Apache-2.0
+ */
+
+#include <init.h>
+#include <gpio.h>
+
+#define SIM_SELECT_PIN 8
+
+static void select_internal_sim(void)
+{
+	struct device* port = device_get_binding(DT_GPIO_P0_DEV_NAME);
+	if (!port) {
+		return;
+	}
+
+    gpio_pin_configure(port, SIM_SELECT_PIN, GPIO_DIR_OUT);
+    gpio_pin_write(port, SIM_SELECT_PIN, 1);
+}
+
+static int board_actinius_icarus_init(struct device *dev)
+{	
+	ARG_UNUSED(dev);
+
+	select_internal_sim();
+
+	return 0;
+}
+
+SYS_INIT(board_actinius_icarus_init, POST_KERNEL,
+	 CONFIG_KERNEL_INIT_PRIORITY_DEFAULT);
